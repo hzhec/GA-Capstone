219d1abcde42dc3e6a47e0df09d61ee52ce19922
hzhec93 <chzhee@gmail.com>
2023-10-23 04:13:10 +0800

    add: navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+	return (
+		<div className="navbar bg-green-50">
+			<div className="navbar-start text-2xl font-bold mx-4">
+				Object Detection using YOLOv8 Model
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
