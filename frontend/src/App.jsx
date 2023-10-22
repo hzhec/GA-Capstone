89e0a603da10181c290919bc2a0e5256ea2a0308
hzhec93 <chzhee@gmail.com>
2023-10-23 04:12:58 +0800

    add: frontend set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import UploadImageForm from "./components/forms/UploadImageForm";
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<UploadImageForm />
+		</>
+	);
+}
+
+export default App;
