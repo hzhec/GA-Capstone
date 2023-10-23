8793bc318b5339a15fbb77382bc6c974da901002
hzhec93 <chzhee@gmail.com>
2023-10-23 19:32:59 +0800

    update: trackedimages.jsx to display preview and delete btn

--- frontend/src/components/TrackedImages.jsx
+++ frontend/src/components/TrackedImages.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
+import PreviewImage from './PreviewImage';
 
 const TrackedImages = () => {
 	const [allImages, setAllImages] = useState();
 	const [currentPage, setCurrentPage] = useState(1);
 	const imagesPerPage = 10;
+	const [showPreview, setShowPreview] = useState(false);
+	const [uuid, setUuid] = useState();
+	const [checkedImages, setCheckedImages] = useState([]);
 
 	const idxOfLastImage = currentPage * imagesPerPage; // 1 * 10 = 10
 	const idxOfFirstImage = idxOfLastImage - imagesPerPage; // 10 - 10 = 0
@@ -14,7 +18,7 @@ const TrackedImages = () => {
 				return response.json();
 			})
 			.then((data) => {
-				console.log(data.allimages);
+				// console.log(data.allimages);
 				setAllImages(data.allimages);
 			})
 			.catch((err) => {
@@ -24,30 +28,58 @@ const TrackedImages = () => {
 
 	const paginate = (pageNum) => setCurrentPage(pageNum);
 
+	const openPreview = () => {
+		setShowPreview((prev) => !prev);
+	};
+
+	const previewImageHandler = (uuid) => {
+		setUuid(uuid);
+		openPreview();
+	};
+
+	const checkboxChangeHandler = (event, imageUuid) => {
+		if (event.target.checked) {
+			setCheckedImages((prevCheckedImages) => [...prevCheckedImages, imageUuid]);
+		} else {
+			setCheckedImages((prevCheckedImages) =>
+				prevCheckedImages.filter((uuid) => uuid !== imageUuid)
+			);
+		}
+	};
+
 	return (
 		<div className="flex flex-col w-full">
+			<PreviewImage handleToggle={() => openPreview()} open={showPreview} uuid={uuid} />
 			{allImages && (
 				<div className="p-5">
 					<div className="overflow-x-auto">
-						<table className="table w-full mb-8">
+						<table className="table w-full mb-8 items-center">
 							<thead>
 								<tr>
+									<th></th>
 									<th>Id</th>
 									<th>Uuid</th>
 									<th>Uploaded Date</th>
+									<th></th>
 									<th></th>
 								</tr>
 							</thead>
 							<tbody>
 								{allImages.slice(idxOfFirstImage, idxOfLastImage).map((image) => (
 									<tr key={image.uuid}>
-										{/* <th>
+										<th>
 											<label>
-												<input type="checkbox" className="checkbox" />
+												<input
+													type="checkbox"
+													className="checkbox checkbox-sm"
+													onChange={(event) =>
+														checkboxChangeHandler(event, image.uuid)
+													}
+												/>
 											</label>
-										</th> */}
+										</th>
 										<td>
-											<div className="flex items-center space-x-3">
+											<div className="flex items-center">
 												<div className="font-bold">{image.id}</div>
 											</div>
 										</td>
@@ -55,15 +87,43 @@ const TrackedImages = () => {
 											<div className="text-sm">{image.uuid}</div>
 										</td>
 										<td>{image['created_at']}</td>
-										<th>
-											<button className="btn btn-ghost btn-xs">Preview</button>
-										</th>
+										<td>
+											<label
+												htmlFor="preview-image"
+												className="btn btn-ghost btn-xs"
+												onClick={() => previewImageHandler(image.uuid)}
+											>
+												Preview
+											</label>
+										</td>
+										<td className="flex items-center justify-center">
+											<button
+												type="button"
+												className="btn btn-square btn-outline btn-xs btn-error"
+												disabled={!checkedImages.includes(image.uuid)}
+											>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													className="h-6 w-6"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke="currentColor"
+												>
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														strokeWidth="2"
+														d="M6 18L18 6M6 6l12 12"
+													/>
+												</svg>
+											</button>
+										</td>
 									</tr>
 								))}
 							</tbody>
 						</table>
 					</div>
-					<div className="flex justify-center">
+					<div className="flex justify-center my-5">
 						<div className="join">
 							<button
 								className="join-item btn btn-sm"
