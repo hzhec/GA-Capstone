df36c52a71d665c5f1b73816bae820182acc52cd
hzhec93 <chzhee@gmail.com>
2023-10-23 17:04:40 +0800

    update: sidebar to display trackedImages page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -28,12 +28,12 @@ const Sidebar = () => {
 
 				<div>
 					<span className="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
-						<a
-							href="javascript:;"
+						<Link
+							to="/tracked-images"
 							className="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark"
 						>
 							Images
-						</a>
+						</Link>
 					</span>
 				</div>
 			</div>
