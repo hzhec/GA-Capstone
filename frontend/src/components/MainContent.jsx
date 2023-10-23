1cc491f896d2f2b58cabeeeec82ee6b901b38a06
hzhec93 <chzhee@gmail.com>
2023-10-23 15:17:09 +0800

    add: maincontent.jsx

---
+++ frontend/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+const MainContent = () => {
+	return (
+		<div className="flex flex-wrap ml-5 w-full">
+			<div className="w-full max-w-full sm:w-1/4 mx-auto text-center">
+				<p className="text-lg text-slate-500 py-1">
+					Tailwind CSS Component
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default MainContent;
